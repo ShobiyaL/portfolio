2eadda5c34ec3d560b2205c2e516fa022a96db71
ShobiyaL <105439836+ShobiyaL@users.noreply.github.com>
2022-12-04 14:02:27 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -12,24 +12,24 @@ function Projects (){
          <h2>Projects</h2>
          <div className="container projects__container">
            
+//             <article className="projects__item">
+//                 <div className="projects__item-image">
+//                   <img src={projectImage} alt="..." />
+//                 </div>
+//                 <h3>Title</h3>
+//                 <div className="projects__cta">
+//                 <a href="http://github.com" target="_blank" className="btn">Github</a>
+//                 <a href="https://dribble.com/Alien_Pixels" target="_blank" className="btn btn-primary">Live-Demo</a>
+//                 </div>
+//             </article>
             <article className="projects__item">
                 <div className="projects__item-image">
                   <img src={projectImage} alt="..." />
                 </div>
                 <h3>Title</h3>
                 <div className="projects__cta">
-                <a href="http://github.com" target="_blank" className="btn">Github</a>
-                <a href="https://dribble.com/Alien_Pixels" target="_blank" className="btn btn-primary">Live-Demo</a>
-                </div>
-            </article>
-            <article className="projects__item">
-                <div className="projects__item-image">
-                  <img src={projectImage} alt="..." />
-                </div>
-                <h3>Title</h3>
-                <div className="projects__cta">
-                <a href="http://github.com" target="_blank" className="btn">Github</a>
-                <a href="https://dribble.com/Alien_Pixels" target="_blank" className="btn btn-primary">Live-Demo</a>
+                <a href="https://github.com/ShobiyaL/movie-ticket-app-client/" target="_blank" className="btn">Github</a>
+                <a href="https://courageous-crumble-60e886.netlify.app" target="_blank" className="btn btn-primary">Live-Demo</a>
                 </div>
             </article>
          </div>
