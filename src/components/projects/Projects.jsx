742fb8ea06f5688983c47f73573a7c1ccc06e3a6
ShobiyaL <105439836+ShobiyaL@users.noreply.github.com>
2022-12-04 14:06:35 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,7 +11,16 @@ function Projects (){
          <h5>My Recent Work</h5>
          <h2>Projects</h2>
          <div className="container projects__container">
-           
+           <article className="projects__item">
+                <div className="projects__item-image">
+                  <img src={projectImage} alt="..." />
+                </div>
+                <h3>Title</h3>
+                <div className="projects__cta">
+                <a href="https://github.com/ShobiyaL/movie-ticket-app-client/" target="_blank" className="btn">Github</a>
+                <a href="https://courageous-crumble-60e886.netlify.app" target="_blank" className="btn btn-primary">Live-Demo</a>
+                </div>
+            </article>
             <article className="projects__item">
                 <div className="projects__item-image">
                   <img src={projectImage} alt="..." />
@@ -22,16 +31,7 @@ function Projects (){
                 <a href="https://dribble.com/Alien_Pixels" target="_blank" className="btn btn-primary">Live-Demo</a>
                 </div>
             </article>
-            <article className="projects__item">
-                <div className="projects__item-image">
-                  <img src={projectImage} alt="..." />
-                </div>
-                <h3>Title</h3>
-                <div className="projects__cta">
-                <a href="https://github.com/ShobiyaL/movie-ticket-app-client/" target="_blank" className="btn">Github</a>
-                <a href="https://courageous-crumble-60e886.netlify.app" target="_blank" className="btn btn-primary">Live-Demo</a>
-                </div>
-            </article>
+            
          </div>
         </section>
     )
