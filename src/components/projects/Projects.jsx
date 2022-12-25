c162be10f4f276495659160815b27e54fe497d70
sofy sofya <sofy7468612@gmail.com>
2022-12-25 21:29:27 +0530

    works good

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -12,14 +12,16 @@ function Projects (){
          <h2>Projects</h2>
          <div className="container projects__container">
            
-            <article className="projects__item">
+         <article className="projects__item">
                 <div className="projects__item-image">
                   <img src={projectImage} alt="..." />
                 </div>
-                <h3>Equipment-Rental-App</h3>
+                <h3>Movie-Ticket-Booking</h3>
                 <div className="projects__cta">
-                <a href="http://github.com" target="_blank" className="btn">Github</a>
-                <a href="https://dribble.com/Alien_Pixels" target="_blank" className="btn btn-primary">Live-Demo</a>
+                <a href="https://courageous-crumble-60e886.netlify.app/" target="_blank" className="btn btn-primary">Live-Demo</a>
+                <a href="https://github.com/ShobiyaL/Movie-Ticket-Application-backend" target="_blank" className="btn ">Back-end</a>
+                <a href="https://github.com/ShobiyaL/movie-ticket-app-client/" target="_blank" className="btn">Front-end</a>
+
                 </div>
             </article>
             <article className="projects__item">
@@ -31,6 +33,18 @@ function Projects (){
                 <a href="https://whimsical-platypus-47d221.netlify.app/" target="_blank" className="btn btn-primary">Live-Demo</a>
                 <a href="https://github.com/ShobiyaL/chat-app-backend.git" target="_blank" className="btn ">Back-end</a>
                 <a href="https://github.com/ShobiyaL/chat-app-client.git" target="_blank" className="btn">Front-end</a>
+
+                </div>
+            </article>
+            <article className="projects__item">
+                <div className="projects__item-image">
+                  <img src={projectImage} alt="..." />
+                </div>
+                <h3>Equipment-Rent-App</h3>
+                <div className="projects__cta">
+                <a href="https://equip-rental-dfbf7e.netlify.app/" target="_blank" className="btn btn-primary">Live-Demo</a>
+                <a href="https://github.com/ShobiyaL/equipment-rental-app-server.git" target="_blank" className="btn ">Back-end</a>
+                <a href="https://github.com/ShobiyaL/equipment-rental-client.git" target="_blank" className="btn">Front-end</a>
 
                 </div>
             </article>
