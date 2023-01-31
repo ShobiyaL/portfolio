89074bc448d00d7cb5d02e0cd399455d7db12b71
sofy sofya <sofy7468612@gmail.com>
2023-01-31 16:39:38 +0530

    upload

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
- import CV from "../../assets/Resume1.pdf"
+//  import CV from "../../assets/Resume1.pdf"
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV}  className="btn" download>Download CV</a>
+        <a href="https://drive.google.com/file/d/16JVNue5YBZeNVkjHL6Zzv32tBMf6YNQh/view?usp=sharing" target='_blank'  className="btn" download>Resume</a>
         <a href="#contact" className="btn primary">Let's Talk</a>
     </div>
   )
