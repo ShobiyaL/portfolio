1b443a871438b7da30775628d9c43db752976abc
ShobiyaL <sofy7468612@gmail.com>
2023-08-25 12:29:59 +0530

    changed resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
 //  import CV from "../../assets/Resume1.pdf"
 const CTA = () => {
   return (
-    <div className="cta">
-        <a href="https://drive.google.com/file/d/16JVNue5YBZeNVkjHL6Zzv32tBMf6YNQh/view?usp=sharing" target='_blank'  className="btn" download>Resume</a>
-        <a href="#contact" className="btn primary">Let's Talk</a>
+    <div className='cta'>
+      <a
+        href='https://drive.google.com/file/d/17HcXB1LTPGxx19zryh9FVFtXdUX407lM/view?usp=drive_link'
+        target='_blank'
+        className='btn'
+        download
+      >
+        Resume
+      </a>
+      <a href='#contact' className='btn primary'>
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
